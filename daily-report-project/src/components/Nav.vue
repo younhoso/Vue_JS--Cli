c1c630bed0707990455f57351e289af7a52fb034
younhoso <thdbsgh3443@naver.com>
2019-08-19 11:36:58 +0900

    190819 커밋

---
+++ daily-report-project/src/components/Nav.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+         <nav class="nav">
+             <ul class="nav-list">
+                 <li>
+                     <button @click="today">
+                         <i class="xi-home"></i>
+                        <span>오늘</span>
+                     </button>
+                 </li>
+                 <li>
+                     <router-link to="/calendar">
+                        <i class="xi-calendar"></i>
+                        <span>달력</span>
+                     </router-link>
+                 </li>
+                 <li>
+                     <router-link to="/setting">
+                        <i class="xi-cog"></i>
+                        <span>설정</span>
+                     </router-link>
+                 </li>
+                 <li>
+                     <router-link to="/about">
+                        <i class="xi-emoticon-smiley"></i>
+                        <span>소개</span>
+                     </router-link>
+                 </li>
+                 
+             </ul>
+            
+            <!-- <router-link to="/about">달력</router-link> |
+            <router-link to="/about">About</router-link> -->
+        </nav>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+    name: 'Nav',
+    methods: {
+        today(){
+            let todayUrl = '/day/' + moment(new Date()).format('YYYY-MM-DD');
+            // console.log(todayUrl);
+            this.$router.push(todayUrl);
+        }
+    },
+    computed: {},
+    data() {
+        return {}
+    },
+    props: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
